7ce41a624fb00e3a8a47e3ac0a33a8e2b3835b0e
andresfdel17 <andresfdel13@hotmail.com>
2023-05-08 21:02:21 -0500

    ajustes de appi

---
+++ Adalberto/API/AppAPIV2/AppAPIV2/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using AppAPIV2.Data;
+using AppAPIV2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPIV2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        // GET api/<controller>
+        public List<Usuario> Get()
+        {
+            return UsuarioData.Listar();
+        }
+        // GET api/<controller>/5
+        public List<Usuario> Get(string id)
+        {
+            return UsuarioData.Obtener(id);
+        }
+        // POST api/<controller>
+        public bool Post([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.registrarUsuario(oUsuario);
+        }
+        // PUT api/<controller>/5
+        public bool Put([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.actualizarUsuario(oUsuario);
+        }
+        // DELETE api/<controller>/5
+        public bool Delete(string id)
+        {
+            return UsuarioData.eliminarUsuario(id);
+        }
+    }
+}
---
+++ Adalberto/API/AppAPIV2/AppAPIV2/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace AppAPIV2.Models
+{
+    public class Usuario
+    {
+        public int IdU { get; set; }
+        public string Nombres { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Ciudad { get; set; }
+        public DateTime FechaIngreso { get; set; }
+    }
+}
